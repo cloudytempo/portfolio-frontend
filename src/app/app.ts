e7ed9d91bf3210a974c02775507a51df930bb56a
DESKTOP-AFP5OIC\Annas <muhammadkhairunnas.abdrahman@gmail.com>
2025-07-06 23:51:13 +0800

    dev-push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, ViewChild } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatListModule, MatIconModule, CommonModule, MatToolbarModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'portfolio-frontend';
+  private route = inject(ActivatedRoute);
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  user: any
+
+  folders: any[] = [
+    {
+      icon: 'home',
+      name: 'Home',
+      position: 'top',
+    },
+    {
+      icon: 'photo_library',
+      name: 'Gallery',
+      position: 'top',
+    },
+    {
+      icon: 'event_note',
+      name: 'Events',
+      position: 'top',
+    },
+    {
+      icon: 'payment_arrow_down',
+      name: 'Expenses',
+      position: 'top',
+    },
+    {
+      icon: 'settings',
+      name: 'Settings',
+      position: 'bottom',
+    },
+        {
+      icon: 'logout',
+      name: 'Log Out',
+      position: 'bottom',
+    },
+  ];
+
+  topFolders = this.folders.filter(f => f.position === 'top');
+  bottomFolders = this.folders.filter(f => f.position === 'bottom');
+  
+    constructor(){
+      const snapshot = this.route.snapshot;
+      console.log(snapshot.url)
+      // this.sidenav.toggle();
+    }
+
+    onActivate($ev: any){
+    console.log($ev)
+  }
+
+    toggleMenu() {
+
+  }
+
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Api } from "../../service/api/api";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatSidenavModule, 
+    MatListModule, 
+    MatIconModule, 
+    MatCardModule, 
+    MatProgressBarModule, 
+    MatProgressSpinnerModule, 
+    MatDatepickerModule, 
+    MatNativeDateModule,
+    CommonModule
+],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
@@ -11,13 +30,37 @@ export class Home {
 
   users: any = [];
 
+  homework = [
+    { name: 'Maths', progress: 50 },
+    { name: 'Physics', progress: 70 },
+    { name: 'English', progress: 23 },
+    { name: 'Biology', progress: 80 }
+  ];
+
+  documents = [
+    { name: 'Presentation.ppt', icon: 'slideshow', date: '22 Sept 2021' },
+    { name: 'Document.docs', icon: 'description', date: '16 Sept 2021' },
+    { name: 'Excel Sheet', icon: 'grid_on', date: '12 Sept 2021' }
+  ];
+
+  schedule = [
+    { subject: 'Literature', chapter: 'Chapter #2 & 3', date: '21 Sept 2021' },
+    { subject: 'Mathematic', chapter: 'Chapter #2 & 3', date: '21 Sept 2021' },
+    { subject: 'English', chapter: 'Chapter #2 & 3', date: '21 Sept 2021' }
+  ];
+
+  feedbacks = [
+    { message: 'I am glad that you completed your weekly homework.', date: '12 Sept 2021' },
+    { message: 'Keep it up and try to do more practice.', date: '12 Sept 2021' }
+  ];
+
   constructor(private userService: Api) {}
 
   async ngOnInit() {
-    await this.userService.getUsers().then(data => {
-      let temp: any = data;
-      this.users = temp[0]
-    })
+    // await this.userService.getUsers().then(data => {
+    //   let temp: any = data;
+    //   this.users = temp[0]
+    // })
 
     console.log(this.users)
   }
